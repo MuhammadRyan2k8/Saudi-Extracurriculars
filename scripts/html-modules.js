17b499024862680766f1e028dd14de8eaabd1289
MuhammadRyan2k8 <132077392+MuhammadRyan2k8@users.noreply.github.com>
2024-08-17 14:19:34 +0300

    Add files via upload

--- scripts/html-modules.js
+++ scripts/html-modules.js
@@ -21,23 +21,19 @@ class MyHeader extends HTMLElement
                 <nav>
 
                     <ul class="top-bar">
-                        <li class="hide-on-mobile" > <a href="#" >Academics</a> </li>
-                        <li class="hide-on-mobile" > <a href="#" >Admission</a> </li>
-                        <li class="hide-on-mobile" > <a href="#" >Research</a> </li>
-                        <li class="hide-on-mobile" > <a href="#" >Athletics</a> </li>
-                        <li class="hide-on-mobile" > <a href="#" >Alumni</a> </li>
-                        <li class="hide-on-mobile" > <a href="#" >Giving</a> </li>
+                        <li class="hide-on-mobile" > <a href="index.html" >Home</a> </li>
+                        <li class="hide-on-mobile" > <a href="our-team.html" >Our Team</a> </li>
+                        <li class="hide-on-mobile" > <a href="upcomming-events.html" >Upcomming Events</a> </li>
+                        <li class="hide-on-mobile" > <a href="join-form.html" >Join</a> </li>
                         <li class="hide-on-pc" onclick=showSidebar() > <a href="#" ><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a> </li>
                     </ul>
                     
                     <ul class="sidebar">
                         <li onclick=hideSidebar() > <a href="#" ><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a> </li>
-                        <li> <a href="#" >Academics</a> </li>
-                        <li> <a href="#" >Admission</a> </li>
-                        <li> <a href="#" >Research</a> </li>
-                        <li> <a href="#" >Athletics</a> </li>
-                        <li> <a href="#" >Alumni</a> </li>
-                        <li> <a href="#" >Giving</a> </li>
+                        <li> <a href="index.html" >Home</a> </li>
+                        <li> <a href="our-team.html" >Our Team</a> </li>
+                        <li> <a href="upcomming-events.html" >Upcomming Events</a> </li>
+                        <li> <a href="join-form.html" >Join</a> </li>
                     </ul>
 
                 </nav>
